146b3286c4c5b8b1a1f6bf0bcc8149c9f2351d23
asmithasatesh <asmithasatesh@gmail.com>
2021-09-15 13:14:07 +0530

    [Asmitha] Added: Add Valiation for first name and last name

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -12,9 +12,40 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
     this.RegisterForm = new FormGroup({
-      firstName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{2,}$'), Validators.minLength(3)]),
-      lastName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{2,}$'), Validators.minLength(3)])
+      firstName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'),Validators.minLength(3)]),
+      lastName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'), Validators.minLength(3)]),
     })
   }
-
+  FirstNameValidation()
+  {
+    if(this.RegisterForm.get('firstName')?.hasError('required'))
+    {
+      return "Enter First Name";
+    }
+    else if(this.RegisterForm.get('firstName')?.hasError('pattern'))
+    {
+      return "Firstname should start with Capital letter";
+    }
+    else if(this.RegisterForm.get('firstName')?.errors?.minlength)
+    {
+      return "Should have minimum 3 letters";
+    }
+    return null;
+  }
+  LastNameValidation()
+  {
+    if(this.RegisterForm.get('lastName')?.hasError('required'))
+    {
+      return "Enter Last Name";
+    }
+    else if(this.RegisterForm.get('lastName')?.hasError('pattern'))
+    {
+      return "Lastname should start with Capital letter";
+    }
+    else if(this.RegisterForm.get('lastName')?.errors?.minlength)
+    {
+      return "Should have minimum 3 letters";
+    }
+    return null;
+  }
 }
