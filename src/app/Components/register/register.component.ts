c5794a6b9aca409a99872afb2c45cd013ba8efcb
asmithasatesh <asmithasatesh@gmail.com>
2021-09-15 16:41:37 +0530

    [Asmitha] Added: Add card border css, validation for password

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -8,13 +8,34 @@ import{ FormControl, Validators, FormGroup} from '@angular/forms';
 export class RegisterComponent implements OnInit {
  RegisterForm!: FormGroup;
  hide = false
+ errorMessafe=""
   constructor() { }
 
   ngOnInit(): void {
     this.RegisterForm = new FormGroup({
-      firstName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'),Validators.minLength(3)]),
-      lastName: new FormControl( '' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'), Validators.minLength(3)]),
+      firstName: new FormControl('',[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'),Validators.minLength(3)]),
+      lastName: new FormControl('' ,[Validators.required, Validators.pattern('^[A-Z]{1}[a-z]{1,}$'), Validators.minLength(3)]),
+      email: new FormControl('',[Validators.email,Validators.required]),
+      password: new FormControl('',[Validators.required,Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^.,:;!@#$%^&*_+=|(){}[?-]*[.,:;!@#$%^&*_+=|(){}[?-][^.,:;!@#$%^&*_+=|(){}[?-]*$).{4,}$'),Validators.minLength(8)]),
+      confirmPassword: new FormControl('',[Validators.required])
     })
+  }
+  ConfirmPasswordValidate()
+  {
+    if(this.RegisterForm.get('confirmPassword')?.hasError('required'))
+    {
+      return "Enter Confirm Password";
+    }
+    else{
+      const control = this.RegisterForm.get('password')?.value;
+      const matchingControl = this.RegisterForm.get('confirmPassword')?.value;
+      if (control == matchingControl) 
+      {
+        console.log(matchingControl);
+        this.errorMessafe = "Those passwords didn’t match. Try again.";
+      }
+      return null;
+    }     
   }
   FirstNameValidation()
   {
@@ -45,6 +66,34 @@ export class RegisterComponent implements OnInit {
     else if(this.RegisterForm.get('lastName')?.errors?.minlength)
     {
       return "Should have minimum 3 letters";
+    }
+    return null;
+  }
+  EmailValidation()
+  {
+    if(this.RegisterForm.get('email')?.hasError('required'))
+    {
+      return "Enter Email";
+    }
+    if(this.RegisterForm.get('email')?.hasError('email'))
+    {
+      return "Email is not in proper format";
+    }
+    return null;
+  }
+  PasswordValidation()
+  {
+    if(this.RegisterForm.get('password')?.hasError('required'))
+    {
+      return "Enter Password";
+    }
+    else if(this.RegisterForm.get('password')?.hasError('pattern'))
+    {
+      return "Please enter a valid Password ";
+    }
+    else if(this.RegisterForm.get('password')?.errors?.minlength)
+    {
+      return "Should have minimum 8 characters";
     }
     return null;
   }
