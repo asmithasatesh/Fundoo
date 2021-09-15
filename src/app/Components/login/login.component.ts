94206ecb72847a73b1c16d8db8a788b211677246
asmithasatesh <asmithasatesh@gmail.com>
2021-09-15 17:30:24 +0530

    [Asmitha] Added: Add Validation for email in Login page

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import{ FormControl, Validators, FormGroup} from '@angular/forms';
 
+  
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+LoginForm!: FormGroup;
 hide = false
   constructor() { }
 
   ngOnInit(): void {
+    this.LoginForm= new FormGroup({
+      email: new FormControl('',[Validators.email,Validators.required]),
+      })
   }
-
+  EmailValidation()
+  {
+    if(this.LoginForm.get('email')?.hasError('required'))
+    {
+      return "Enter Email";
+    }
+    if(this.LoginForm.get('email')?.hasError('email'))
+    {
+      return "Email is not in proper format";
+    }
+    return null;
+  }
 }
