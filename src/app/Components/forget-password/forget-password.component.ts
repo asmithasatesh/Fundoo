bb33d727a02d59f991fed19bcf87fe78476ace17
asmithasatesh <asmithasatesh@gmail.com>
2021-09-15 22:32:59 +0530

    [Asmitha] Added: Add email validation in forget password form

--- src/app/Components/forget-password/forget-password.component.ts
+++ src/app/Components/forget-password/forget-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ FormControl, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-forget-password',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forget-password.component.scss']
 })
 export class ForgetPasswordComponent implements OnInit {
-
+  ForgetPasswordForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.ForgetPasswordForm= new FormGroup({
+      email: new FormControl('',[Validators.email,Validators.required])
+  })
+};
+EmailValidation()
+{
+  if(this.ForgetPasswordForm.get('email')?.hasError('required'))
+  {
+    return "Enter Email";
   }
-
+  if(this.ForgetPasswordForm.get('email')?.hasError('email'))
+  {
+    return "Email is not in proper format";
+  }
+  return null;
+}
 }
