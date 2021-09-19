f4319fc06518a0094f78bb0d7ca14d18720eccc0
asmithasatesh <asmithasatesh@gmail.com>
2021-09-19 20:06:01 +0530

    [Asmitha] Refactor: Add Scss and mouseover feature

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -33,9 +33,7 @@ export class DashboardComponent implements OnInit {
   }
   Toggle()
   {
-    if(this.toggle!=true || !(this.toggle==true && this.opened == true))
-    {
-      this.opened=!this.opened;
+      this.toggle=!this.toggle;
     }
-  }
+  
 }
